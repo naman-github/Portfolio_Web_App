afa0fdb9131295ab8307c8116b8af47b7563e97b
Naman Sharma <naman.ns23@gmail.com>
2024-12-05 23:36:43 +0530

    Updated: Reusable Components and Base Setup Major

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <main>homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br /> <span className="text-accent">Naman Sharma</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant degital experiences that captivate users and drive business success.</p>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
+  );
+}; 
+
+export default Home;
